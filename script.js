f9a810e84468591e7144654e4c817b0683c4ce74
Heron Slovinski <102620093+herongs@users.noreply.github.com>
2022-07-26 07:55:57 -0300

    projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+let inputAdultos = document.getElementById("adultos")
+let inputCriancas = document.getElementById("criancas")
+let inputDuracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calcular(){
+    console.log("Calculando")
+}
